9e86af8bd1875be4f0de5eb1be659a564c5f7e50
Manavjot Singh <github@hasmailed.com>
2023-07-18 09:52:51 +0200

    feat: configure 1password gui

--- nixos/_mixins/desktop/1password.nix
+++ nixos/_mixins/desktop/1password.nix
@@ -1,6 +1,9 @@
-{ lib, pkgs, ... }: {
+{ lib, pkgs, username, ... }: {
   programs = {
-    _1password-gui.enable = true;
+    _1password-gui = {
+      enable = true;
+      polkitPolicyOwners = [ "${username}" ];
+    };
     _1password.enable = true;
   };
 }
