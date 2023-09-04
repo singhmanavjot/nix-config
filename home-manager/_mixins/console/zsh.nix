0d1a80367c018b209c9038951b00bf0b60c47590
Manavjot Singh <github@hasmailed.com>
2023-09-04 11:45:07 +0200

    feat: add argocd aliases

--- home-manager/_mixins/console/zsh.nix
+++ home-manager/_mixins/console/zsh.nix
@@ -8,5 +8,10 @@
       enable = true;
       plugins = [ "git" "kubectl" "minikube" "direnv" "terraform" "1password" ];
     };
+
+    shellAliases = {
+      argo-pw = "kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath=\"{.data.password}\" | base64 -d | xargs echo";
+      argo-tunnel = "kubectl port-forward -n argocd services/argo-cd-argocd-server 8080:80";
+    };
   };
 }
--- nixos/_mixins/virt/virt-manager.nix
+++ nixos/_mixins/virt/virt-manager.nix
@@ -3,3 +3,4 @@
 virtualisation.libvirtd.enable = true;
 programs.dconf.enable = true;
 environment.systemPackages = with pkgs; [ virt-manager ];
+}
