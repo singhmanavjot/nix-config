ced0ef704358d9936ce963a68983b4e8ed2d7f78
Manavjot Singh <64074588+singhmanavjot@users.noreply.github.com>
2025-09-29 19:09:19 +0200

    feat(sops): remove commented example from sops.nix

--- home-manager/sops.nix
+++ home-manager/sops.nix
@@ -5,12 +5,5 @@
     defaultSopsFormat = "yaml";
 
     age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
-
-    # Example secret configuration:
-    # secrets = {
-    #   "my_secret_key" = {
-    #     path = "${config.home.homeDirectory}/.config/my_app/secret";
-    #   };
-    # };
   };
 }
