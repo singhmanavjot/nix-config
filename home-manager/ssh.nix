0aca05f61eb3683bce07b3fc01efc55a9be557c9
Manavjot Singh <64074588+singhmanavjot@users.noreply.github.com>
2025-10-19 10:57:23 +0200

    chore: add ssh config for homelab

--- home-manager/ssh.nix
+++ home-manager/ssh.nix
@@ -8,6 +8,11 @@
         user = "git";
         identityFile = "~/.ssh/github_auth_key";
       };
+      "homelab" = {
+        hostname = "192.168.13.200";
+        user = "manav";
+        identityFile = "~/.ssh/homelab";
+      };
     };
   };
 }
